045711b6870b40c13768226b1e3dc502662da6b2
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2019-02-11 15:46:17 -0800

    attempting go... Python backend works

---
+++ python/server.py
@@ -0,0 +1,53 @@
+import grpc
+import time
+import hashlib
+import basicmessage_pb2
+import basicmessage_pb2_grpc
+from concurrent import futures
+ 
+class backendServer(basicmessage_pb2_grpc.backendServicer):
+    """
+    gRPC server 
+    """
+    def __init__(self, *args, **kwargs):
+        self.server_port = 55001
+ 
+    def sendMessage(self, request, context):
+        """
+        Implementation of the rpc sendMessage declared in the proto
+        file above.
+        """
+        # get the string from the incoming request
+        input = request.backendRequest
+        result = input
+  
+        return basicmessage_pb2.backendReply(**input)
+ 
+    def start_server(self):
+        """
+        Function which actually starts the gRPC server, and preps
+        it for serving incoming connections
+        """
+        # declare a server object with desired number
+        # of thread pool workers.
+        backendServer = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+ 
+        # bind the server to the port defined above
+        backendServer.add_insecure_port('[::]:{}'.format(self.server_port))
+ 
+        # start the server
+        backendServer.start()
+        print ('Server running ...')
+ 
+        try:
+            # need an infinite loop since the above
+            # code is non blocking, and if I don't do this
+            # the program will exit
+            while True:
+                time.sleep(60*60*60)
+        except KeyboardInterrupt:
+            backendServer.stop(0)
+            print('Digestor Server Stopped ...')
+ 
+curr_server = backendServer()
+curr_server.start_server()
