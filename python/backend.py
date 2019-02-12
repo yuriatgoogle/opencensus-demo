dafd64f53ab6239aca2ebf458f3ed0a01ae7604c
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2019-02-12 09:43:43 -0800

    python grpc finally works!

--- python/backend.py
+++ python/backend.py
@@ -1,49 +1,42 @@
+# service = grpcBackend
+ # method = returnValue
+ # message = grpcMessage
+ # field = payload
+
 import grpc
-import time
-import hashlib
-import basicmessage_pb2
-import basicmessage_pb2_grpc
 from concurrent import futures
- 
-class backendServer(basicmessage_pb2_grpc.backendServicer):
-    """
-    gRPC server 
-    """
-    def __init__(self, *args, **kwargs):
-        self.server_port = 55001
- 
-    def sendMessage(self, request, context):
-        """
-        Implementation of the rpc sendMessage declared in the proto
-        file above.
-        """ 
-        return basicmessage_pb2.backendReply(request.backendRequest)
- 
-    def start_server(self):
-        """
-        Function which actually starts the gRPC server, and preps
-        it for serving incoming connections
-        """
-        # declare a server object with desired number
-        # of thread pool workers.
-        backendServer = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
- 
-        # bind the server to the port defined above
-        backendServer.add_insecure_port('[::]:{}'.format(self.server_port))
- 
-        # start the server
-        backendServer.start()
-        print ('Server running ...')
- 
-        try:
-            # need an infinite loop since the above
-            # code is non blocking, and if I don't do this
-            # the program will exit
-            while True:
-                time.sleep(60*60*60)
-        except KeyboardInterrupt:
-            backendServer.stop(0)
-            print('Digestor Server Stopped ...')
- 
-curr_server = backendServer()
-curr_server.start_server()
+import time
+
+# import the generated classes
+import grpcDemo_pb2
+import grpcDemo_pb2_grpc
+
+# create a class to define the server functions
+# derived from calculator_pb2_grpc.CalculatorServicer
+class grpcDemoServicer(grpcDemo_pb2_grpc.grpcBackendServicer):
+
+    def returnValue(self, request, context):
+        # response = request.payload
+        response = grpcDemo_pb2.grpcMessage()
+        response.payload = request.payload
+        return response
+
+# create a gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# add defined class to server
+grpcDemo_pb2_grpc.add_grpcBackendServicer_to_server(
+    grpcDemoServicer(), server)
+
+# listen on port 50051
+print('Starting server. Listening on port 50051.')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+# since server.start() will not block,
+# a sleep-loop is added to keep alive
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
--- python/frontend.py
+++ python/frontend.py
@@ -1,16 +1,30 @@
+ # service = grpcBackend
+ # method = returnValue
+ # message = grpcMessage
+ # field = payload
+
 import grpc
 
 # import the generated classes
-import basicmessage_pb2
-import basicmessage_pb2_grpc
+import grpcDemo_pb2
+import grpcDemo_pb2_grpc
 
 # open a gRPC channel
-channel = grpc.insecure_channel('localhost:55001')
+channel = grpc.insecure_channel('localhost:50051')
 
 # create a stub (client)
-backend = basicmessage_pb2_grpc.backendStub(channel)
-message = basicmessage_pb2.commString(payload='test')
+stub = grpcDemo_pb2_grpc.grpcBackendStub(channel)
+# stub = calculator_pb2_grpc.CalculatorStub(channel)
 
-response = backend.sendMessage(message)
+# create a valid request message
+# message to send = pb2.message(field name = field value)
+sendString = grpcDemo_pb2.grpcMessage(payload='test')
+# number = calculator_pb2.Number(value=16)
 
-print(response.value)
+# make the call
+# response = stub.SquareRoot(number)
+# response = stub.method(message to send)
+response = stub.returnValue(sendString)
+
+# return
+print(response.payload)
